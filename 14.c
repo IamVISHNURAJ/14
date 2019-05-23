122a5d10218c791f591cbf8cd0aabd737651111b
IamVISHNURAJ <50854010+IamVISHNURAJ@users.noreply.github.com>
2019-05-23 16:23:00 +0530

    Update 14.c

--- 14.c
+++ 14.c
@@ -1,15 +1,14 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
- int i,n,m;
-    scanf("%d", &n,&m);
-    if(n%2==0)
-    {
-      n++;
-    }
-    for(i=n; i<=m; i+=2)
-    {
-        printf("%d\n", i);
-    }
-    return 0;
+int n,m,i;
+scanf("%d %d",&n,&m);
+for(i=n+1;i<m;i++)
+{
+    if(i%2!=0)
+{
+    printf("%d ",i);
+}
+}
+return 0;   
 }
